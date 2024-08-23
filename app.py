b3a776fec9a2ad4a6d8c106eb7da127ed7e6416d
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-08-23 12:17:51 +0300

    Add create_task view to app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, Response, flash
 
+SECRET_KEY = "dev"
 app = Flask(__name__)
+app.config["SECRET_KEY"] = SECRET_KEY
 
 fake_db = [
     {
@@ -21,9 +23,30 @@ fake_db = [
 ]
 
 
-@app.route("/")
-def index() -> render_template:
+def get_id_for_adding(db: list[dict]) -> int:
+    if len(db) == 0:
+        return 1
+    return max([task["id"] for task in db]) + 1
+
+
+@app.route("/", methods=["GET"])
+def index() -> str:
     return render_template("index.html", task_list=fake_db)
+
+
+@app.route("/task/create", methods=["POST"])
+def create_task() -> Response:
+    task_name = request.form.get("name", "").strip()
+    if task_name:
+        fake_db.append({
+            "id": get_id_for_adding(fake_db),
+            "name": task_name,
+            "done": False
+        })
+        flash(f"Task created", "success")
+    else:
+        flash("Please enter a correct name", "danger")
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
